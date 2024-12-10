07d656c86b11e6fd0e7bf021170448a20014e56c
chu ding <cding91@gmail.com>
2024-12-10 15:57:59 -0600

    Remove unused input/output

--- Src/DiffusionService/ServiceOutput.cs
+++
@@ -1,9 +0,0 @@
-namespace DiffusionService;
-
-/// <summary>
-/// Output for the diffusion model. Must be compatible with the model API.
-/// </summary>
-public record ServiceOutput
-{
-    public string ImageUrl { get; init; } = string.Empty;
-}
